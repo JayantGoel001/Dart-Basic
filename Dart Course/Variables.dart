0ee572a9056daf37072c8fdbdb877a20daff967c
CormacKrum <jgoel92@gmail.com>
2020-08-16 03:42:07 +0530

    Initial commit

---
+++ Dart Course/Variables.dart
@@ -0,0 +1,13 @@
+main(){
+  var age= 18;
+  var name = "Hello";
+  var isMale = true;
+  var weight = 5.4;
+  age+=7;
+  print(age);
+  print(name);
+  print(isMale);
+  print(weight);
+
+  print("$name is $age years old with weight of $weight kgs");
+}
