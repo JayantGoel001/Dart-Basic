4e63cdd411b66de22ee6e4ce20ea85b389ce9366
CormacKrum <jgoel92@gmail.com>
2020-08-18 14:40:27 +0530

    Initial commit

--- Dart Course/Arithmetic Operators.dart
+++ Dart Course/Arithmetic Operators.dart
@@ -1,3 +1,4 @@
+import 'dart:math';
 main(){
 
   var num1 = 10;
@@ -16,5 +17,21 @@ main(){
 
   var mod = num1 % num2;
   print(mod);
+
+  var base = 10;
+  var height = 5;
+
+  var area = 0.5 * base * height;
+  print(area);
+
+  var avg = 5 + 6 + 7 + 8/4;
+  print(avg);
+
+  var avg2 = (5 + 6 + 7 + 8)/4;
+  print(avg2);
+
+  var a = 5;
+  var area2 = 6 * pow(a,2);
+  print(area2);
 
 }
