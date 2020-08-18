2dfbec4280f690bfffaafcfdb547a12add1986bd
CormacKrum <jgoel92@gmail.com>
2020-08-18 14:19:24 +0530

    Initial commit

---
+++ Dart Course/Arithmetic Operators.dart
@@ -0,0 +1,20 @@
+main(){
+
+  var num1 = 10;
+  var num2 = 5;
+  var add = num1 + num2;
+  print(add);
+
+  var sub = num1-num2;
+  print(sub);
+
+  var mul = num1 * num2;
+  print(mul);
+
+  var div = num1/num2;
+  print(div);
+
+  var mod = num1 % num2;
+  print(mod);
+
+}
