88b7b9b146b8e800805df5550eacb92c6a99f7f7
CormacKrum <jgoel92@gmail.com>
2020-08-19 10:17:19 +0530

    Initial commit

--- Dart Course/Arithmetic Operators.dart
+++ Dart Course/Arithmetic Operators.dart
@@ -1,42 +1,42 @@
 import 'dart:math';
 main(){
 
-  var num1 = 10;
-  var num2 = 5;
-  var add = num1 + num2;
-  print(add);
+    var num1 = 10;
+    var num2 = 5;
+    var add = num1 + num2;
+    print(add);
 
-  var sub = num1-num2;
-  print(sub);
+    var sub = num1-num2;
+    print(sub);
 
-  var mul = num1 * num2;
-  print(mul);
+    var mul = num1 * num2;
+    print(mul);
 
-  var div = num1/num2;
-  print(div);
+    var div = num1/num2;
+    print(div);
 
-  var mod = num1 % num2;
-  print(mod);
+    var mod = num1 % num2;
+    print(mod);
 
-  var base = 10;
-  var height = 5;
+    var base = 10;
+    var height = 5;
 
-  var area = 0.5 * base * height;
-  print(area);
+    var area = 0.5 * base * height;
+    print(area);
 
-  var avg = 5 + 6 + 7 + 8/4;
-  print(avg);
+    var avg = 5 + 6 + 7 + 8/4;
+    print(avg);
 
-  var avg2 = (5 + 6 + 7 + 8)/4;
-  print(avg2);
+    var avg2 = (5 + 6 + 7 + 8)/4;
+    print(avg2);
 
-  var a = 5;
-  var area2 = 6 * pow(a,2);
-  print(area2);
+    var a = 5;
+    var area2 = 6 * pow(a,2);
+    print(area2);
 
-  var num3 = 6;
-  num3++;
-  num3--;
-  --num3;
-  print(num3);
+    var num3 = 6;
+    num3++;
+    num3--;
+    --num3;
+    print(num3);
 }
