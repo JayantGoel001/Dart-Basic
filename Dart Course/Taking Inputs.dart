742ef8a025d64f0e5cf9a3a2a1ead0c6134a177b
CormacKrum <jgoel92@gmail.com>
2020-08-19 10:29:00 +0530

    Initial commit

--- Dart Course/Taking Inputs.dart
+++ Dart Course/Taking Inputs.dart
@@ -7,4 +7,9 @@ main(){
   print("Whats Your age?");
   var age = int.parse(stdin.readLineSync());
   print("Coool $age");
+
+  print("Enter a:");
+  var a = int.parse(stdin.readLineSync());
+  var sa = 6*a*a;
+  print("Surface Area : $sa");
 }
