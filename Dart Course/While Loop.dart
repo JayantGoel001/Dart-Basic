6188c0aff105373450a6ad21c8a02c12180400ec
username <54479676+CormacKrum@users.noreply.github.com>
2020-08-26 05:09:40 +0530

    Initial commit

--- Dart Course/While Loop.dart
+++ Dart Course/While Loop.dart
@@ -1,7 +1,15 @@
+import 'dart:io';
 main() {
   var i=0;
   while(i<10){
     print(i);
     i++;
+  }
+
+  print("Enter the Password");
+  var password = stdin.readLineSync();
+  while(password!="***"){
+    print(password);
+    password = stdin.readLineSync();
   }
 }
