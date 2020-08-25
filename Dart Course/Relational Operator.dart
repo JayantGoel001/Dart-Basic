b403232d09ee5687edeea6ada5147da9edb621cc
username <54479676+CormacKrum@users.noreply.github.com>
2020-08-26 04:56:36 +0530

    Initial commit

---
+++ Dart Course/Relational Operator.dart
@@ -0,0 +1,39 @@
+main() {
+  var num1 = 10;
+  var num2 = 20;
+  var num3 = 5;
+
+  if(num1>num2){
+    print("True");
+  }
+  else{
+    print("False");
+  }
+
+  if(num1==num3){
+    print("True");
+  }
+  else{
+    print("False");
+  }
+
+  if(num1>=num2){
+    print("True");
+  }
+  else{
+    print("False");
+  }
+
+  if(num1!=num2){
+    print("True");
+  }
+  else{
+    print("False");
+  }
+
+
+
+
+
+
+}
