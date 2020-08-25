fbbd73cffd1dba5e19eba976df2776e4d0caff25
username <54479676+CormacKrum@users.noreply.github.com>
2020-08-26 04:51:57 +0530

    Initial commit

--- Dart Course/Desicion Making.dart
+++ Dart Course/Desicion Making.dart
@@ -2,10 +2,18 @@ import 'dart:io';
 main(){
   var name = "Mono";
   var name2 = stdin.readLineSync();
-  if(name != null && name2!=null){
+  if(name != null && name2!=null && name == name2){
     print("We are in Hell.");
   }
   else{
     print("Great");
+  }
+
+  var number = int.parse(stdin.readLineSync());
+  if(number%2==0){
+    print("Even");
+  }
+  else{
+    print("Odd");
   }
 }
